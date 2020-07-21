959b92ac92ddc2639cc209c24d0f97a1edfb63ff
Brodie Bosecke <brodie.bosecke.yt@gmail.com>
2020-07-21 20:18:02 +1200

    coded game from pseudocode into commandLine output

--- gameOfLife.cpp
+++ gameOfLife.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
+#include <future>
 
 using std::cout;
 using std::endl;
@@ -14,12 +17,87 @@ using std::endl;
 *
 */
 
+void nextGeneration(int board[10][10])
+{
+    int future[10][10];
 
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 10; j++){
+            int aliveNeighbours = 0;
+            for(int x = -1; x <= 1; x++){
+                for(int y = -1; y <= 1; y++){
+                    aliveNeighbours += board[i+x][j+y];
+                }
+            }
+            aliveNeighbours -= board[i][j];
+            if((board[i][j] == 1) && (aliveNeighbours < 2)){
+                future[i][j] = 0;
+            } else if((board[i][j] == 1) && (aliveNeighbours > 3)){
+                future[i][j] = 0;
+            } else if((board[i][j] == 0) && (aliveNeighbours == 3)){
+                future[i][j] = 1;
+            } else {
+                future[i][j] = board[i][j];
+            }
+        }
+    }
+
+    cout << "Next Generation" << endl;
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 10; j++){
+            if(future[i][j] == 0){
+                cout << ".";
+            } else {
+                cout << "*";
+            }
+        }
+        cout << endl;
+    }
+
+
+}
+
+
+/**
+*
+*
+*Testing pointers and how to access from methods
+*/
 int main()
 {
-    //Build board
+    int board[10][10] = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+        {0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+        {0, 0, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 1, 0, 1, 0, 1, 0, 0, 0},
+        {0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 1, 0, 0, 1, 0, 0, 0},
+    };
+    cout << "Original Generation" << endl;
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 10; j++){
+            if(board[i][j] == 0){
+                cout << ".";
+            } else {
+                cout << "*";
+            }
+        }
+        cout << endl;
+    }
+    int timer = 0;
+    //while(timer != 10){
+    //}
+    nextGeneration(board);
+    
+
+
+
     //Intialise original living tiles, all else is dead
     //Rules per rotation
 
-    return 1;
+    return 0;
 }
