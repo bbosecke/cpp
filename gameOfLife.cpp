a065be1e1c7e41ea889a7d0bcdf3ff3f8f102fc6
Brodie Bosecke <brodie.bosecke.yt@gmail.com>
2020-07-21 19:40:18 +1200

    Outline structure/first commit

---
+++ gameOfLife.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+/**
+* @author Brodie Bosecke
+*   The game Of Life is a well-known computing problem that I am going to replicate
+*   This is the first program I am going to use that has a GUI in cpp
+*
+*   To compile the program, "g++ -o gamePlay gameOfLife.cpp"
+*   To run the program, "./gamePlay"
+*
+*
+*/
+
+
+int main()
+{
+    //Build board
+    //Intialise original living tiles, all else is dead
+    //Rules per rotation
+
+    return 1;
+}
